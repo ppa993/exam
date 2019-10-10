0abffc2d711d58d1865e66df0e90cc1f08d50e74
An Pham <An_Pham_Phuoc@manulife.com>
2019-10-10 09:42:09 +0700

    remove unused

--- gatsby-browser.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import React from 'react'
-import { ThemeProvider } from './src/context/ThemeContext'
-
-export const wrapRootElement = ({ element }) => <ThemeProvider>{element}</ThemeProvider>
