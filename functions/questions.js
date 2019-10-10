a676252b2f8cc092b2a46936bbc90c701d678ffb
An Pham <An_Pham_Phuoc@manulife.com>
2019-10-10 14:01:39 +0700

    add .functions

---
+++ functions/questions.js
@@ -0,0 +1,48 @@
+const questions = [
+  {
+    question: 'cc34fa8f751a4995b4dbfa4d1d484ba3',
+    answer: 'A'
+  },
+  {
+    question: 'cc34fa8f751a4995b4dbfa4d1d484ba4',
+    answer: 'C',
+  },
+  {
+    question: 'cc34fa8f751a4995b4dbfa4d1d484ba5',
+    answer: 'B',
+  },
+  {
+    question: 'cc34fa8f751a4995b4dbfa4d1d484ba6',
+    answer: 'B',
+  },
+  {
+    question: 'cc34fa8f751a4995b4dbfa4d1d484ba7',
+    answer: 'B',
+  },
+  {
+    question: 'cc34fa8f751a4995b4dbfa4d1d484ba8',
+    answer: 'B',
+  },
+  {
+    question: 'cc34fa8f751a4995b4dbfa4d1d484ba9',
+    answer: 'B',
+  },
+  {
+    question: 'cc34fa8f751a4995b4dbfa4d1d484ba0',
+    answer: 'B',
+  },
+  {
+    question: 'cc34fa8f751a4995b4dbfa4d1d484bb1',
+    answer: 'B',
+  },
+  {
+    question: 'cc34fa8f751a4995b4dbfa4d1d484bbb2',
+    answer: 'B',
+  },
+  {
+    question: 'cc34fa8f751a4995b4dbfa4d1d484bbb3',
+    answer: 'B',
+  }
+]
+
+module.exports = questions;
---
+++ functions/router.js
@@ -0,0 +1,58 @@
+﻿var express = require('express');
+var router = express.Router();
+var questions = require('./questions');
+
+
+router.get('/', (req, res) => {
+    res.send('Hello there!');
+});
+
+/*
+ * GET dish list.
+router.get('/', (req, res) => {
+    var result = req.body;
+    var count = 0;
+    result.forEach(item => {
+        if (questions.some(q => q.question === item.question && q.answer === item.answer)) count++;
+    })
+    res.json({result: count});
+});
+ */
+
+/*
+ * GET dish by ID.
+router.get('/:id', function (req, res) {
+    var db = req.db;
+    var collection = db.get('dishes');
+    var articalID = req.params.id;
+    collection.find({ '_id': articalID }, {}, function (e, docs) {
+        res.json(docs);
+    });
+});
+ */
+
+/*
+ * POST to add new dish.
+ */
+router.post('/submit', (req, res) => {
+    var result = req.body.data;
+    var count = 0;
+    result.forEach(item => {
+        if (questions.some(q => q.question === item.question && q.answer === item.answer)) count++;
+    })
+    res.json({result: count, total: questions.length});
+});
+
+/*
+ * DELETE to delete dish.
+router.delete('/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('dishes');
+    var articalToDelete = req.params.id;
+    collection.remove({ '_id': articalToDelete }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+ */
+
+module.exports = router;
