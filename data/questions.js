25ea398b114e38b687c33240e8c784ce1466566b
An Pham <An_Pham_Phuoc@manulife.com>
2019-12-09 08:24:28 +0700

    update questions

--- data/questions.js
+++ data/questions.js
@@ -1,257 +1,234 @@
 const questions =  [
   {
     id: 'cc34fa8f751a4995b4dbfa4d1d484ba3',
-    question: 'Lorem ipsum',
+    question: 'Theo em lí do bà hoàng hậu lại tìm đủ mọi cách để làm hại nàng Bạch Tuyết trong truyện Nàng Bạch Tuyết và bảy chú lùn?',
     answer: 'A',
     answers: [
       {
         label: 'A',
-        value: 'Lorem A'
+        value: 'Trong tâm trí của bà Hoàng hậu luôn tồn tại một niềm tin phi lí rằng “mình là đẹp nhất”.'
       },
       {
         label: 'B',
-        value: 'Lorem B'
+        value: 'Bà Hoàng hậu có tính đố kị, ghét Bạch Tuyết vì nàng không phải là con ruột.'
       },
       {
         label: 'C',
-        value: 'Lorem C'
+        value: 'Bà muốn hãm hại Bạch Tuyết vì nàng ta làm bà khó chịu.'
       },
       {
         label: 'D',
-        value: 'Lorem D'
+        value: 'Bà muốn hại Bạch Tuyết vì nàng ta được nhiều người yêu mến.'
       }
     ]
   },
   {
     id: 'cc34fa8f751a4995b4dbfa4d1d484ba4',
-    question: 'Lorem ipsum dolo',
+    question: 'Theo em trong các tình huống/ phưng án sau đây, phương án nào được xem là đó là cách nhìn nhận và đánh giá vấn đề khách quan, tích cực?',
     answer: 'C',
     answers: [
       {
         label: 'A',
-        value: 'Lorem A'
+        value: 'Tôi không làm được bài vì tôi học không trúng tủ.'
       },
       {
         label: 'B',
-        value: 'Lorem B'
+        value: 'Tôi không làm bài được vì tôi bị ốm.'
       },
       {
         label: 'C',
-        value: 'Lorem C'
+        value: 'Tôi cần cố gắng hơn vào lần kiểm tra tới.'
       },
       {
         label: 'D',
-        value: 'Lorem D'
+        value: 'Tôi không làm được bài vì tôi học yếu môn đó.'
       }
     ]
   },
   {
     id: 'cc34fa8f751a4995b4dbfa4d1d484ba5',
-    question: 'Lorem ipsum amed',
-    answer: 'B',
+    question: 'Suy nghĩ nào sau đây đúng khi làm quen?',
+    answer: 'D',
     answers: [
       {
         label: 'A',
-        value: 'Lorem A'
+        value: 'Chủ động làm quen nếu thích.'
       },
       {
         label: 'B',
-        value: 'Lorem B'
+        value: 'Giao tiếp bằng hành động phi ngôn ngữ để tạo ấn tướng làm quen.'
       },
       {
         label: 'C',
-        value: 'Lorem C'
+        value: 'Chủ động giới thiệu, chia sẻ suy nghĩ của bản thân để làm quen một người nào đó.'
       },
       {
         label: 'D',
-        value: 'Lorem D'
+        value: 'Cởi mở, lịch sự, chủ động bắt chuyện làm quen.'
       }
     ]
   },
   {
     id: 'cc34fa8f751a4995b4dbfa4d1d484ba6',
-    question: 'Lorem ipsum amed',
-    answer: 'B',
+    question: 'Khi tức giận bạn sẽ làm gì trước tiên?',
+    answer: 'A',
     answers: [
       {
         label: 'A',
-        value: 'Lorem A'
+        value: 'Bình tĩnh, nhìn lại sự việc để ngẫm nghĩ.'
       },
       {
         label: 'B',
-        value: 'Lorem B'
+        value: 'Bình tĩnh lại và tìm cách để trả thù người làm mình giận.'
       },
       {
         label: 'C',
-        value: 'Lorem C'
+        value: 'Giận cá chém thớt, đổ cơn giận lên đầu người khác.'
       },
       {
         label: 'D',
-        value: 'Lorem D'
+        value: 'Ngay tức khắc có tư tưởng ăn miếng trả miếng.'
       }
     ]
   },
   {
     id: 'cc34fa8f751a4995b4dbfa4d1d484ba7',
-    question: 'Lorem ipsum amed',
-    answer: 'B',
+    question: 'Khi đứng trước sự lựa chọn bạn sẽ làm gì?',
+    answer: 'C',
     answers: [
       {
         label: 'A',
-        value: 'Lorem A'
+        value: 'Chọn thứ mà mình thích nhưng chỉ có lợi cho bản thân.'
       },
       {
         label: 'B',
-        value: 'Lorem B'
+        value: 'Chọn thứ mà người khác bảo là tốt nhưng bản thân không muốn.'
       },
       {
         label: 'C',
-        value: 'Lorem C'
+        value: 'Chọn thứ mà bản thân cho là đúng sau khi suy nghĩ kĩ càng.'
       },
       {
         label: 'D',
-        value: 'Lorem D'
+        value: 'Do dự không biết nên chọn thứ gì.'
       }
     ]
   },
   {
     id: 'cc34fa8f751a4995b4dbfa4d1d484ba8',
-    question: 'Lorem ipsum amed',
+    question: 'Trong truyện Thạch Sanh tại sao Lí Thông lại chọn làm quen với Thạch Sanh?',
     answer: 'B',
     answers: [
       {
         label: 'A',
-        value: 'Lorem A'
+        value: 'Vì Lí Thông hứng thú với Thạch Sanh.'
       },
       {
         label: 'B',
-        value: 'Lorem B'
+        value: 'Vì Thạch Sanh dễ bị lợi dụng.'
       },
       {
         label: 'C',
-        value: 'Lorem C'
+        value: 'Vì Lí Thông không có bạn bè cần Thạch Sanh làm bầu bạn.'
       },
       {
         label: 'D',
-        value: 'Lorem D'
+        value: 'Vì Lí Thông muốn có bạn kết nghĩa để làm ăn công bằng.'
       }
     ]
   },
   {
     id: 'cc34fa8f751a4995b4dbfa4d1d484ba9',
-    question: 'Lorem ipsum amed',
-    answer: 'B',
+    question: 'Khi bạn muốn mượn một thứ gì đó gấp nhưng người khác khác lại không cho, bạn sẽ làm gì?',
+    answer: 'D',
     answers: [
       {
         label: 'A',
-        value: 'Lorem A'
+        value: 'Sẽ cướp giật từ tay người đó.'
       },
       {
         label: 'B',
-        value: 'Lorem B'
+        value: 'Đợi người đó đi rồi lấy trộm của người đó.'
       },
       {
         label: 'C',
-        value: 'Lorem C'
+        value: 'Nài nỉ, làm phiền người đó cho đến khi người đó cho mượn.'
       },
       {
         label: 'D',
-        value: 'Lorem D'
+        value: 'Nói ra lí do mà mình thực sự cần thiết để người đó thấy phù hợp để cho mình mượn.'
       }
     ]
   },
   {
     id: 'cc34fa8f751a4995b4dbfa4d1d484ba0',
-    question: 'Lorem ipsum amed',
+    question: 'Theo em hành động nào dưới đây thể hiện cách quản lí cảm xúc hiệu quả?',
     answer: 'B',
     answers: [
       {
         label: 'A',
-        value: 'Lorem A'
+        value: 'Khi tức giận kiềm chế, không nói ra.'
       },
       {
         label: 'B',
-        value: 'Lorem B'
+        value: 'Khi tức giận, bình tĩnh nói rõ cho người làm mình tức giận biết rằng mình đang thực sự tức giận và người đó đã sai ở đâu.'
       },
       {
         label: 'C',
-        value: 'Lorem C'
+        value: 'Khi tức giận, khéo léo bằng cách nói móc người làm cho mình tức giận.'
       },
       {
         label: 'D',
-        value: 'Lorem D'
+        value: 'Khi tức giận, trách móc, đem cơn giận dữ của mình đổ lên đầu người khác.'
       }
     ]
   },
   {
     id: 'cc34fa8f751a4995b4dbfa4d1d484bb1',
-    question: 'Lorem ipsum amed',
-    answer: 'B',
+    question: 'Trong cuộc sống, mục đích chính mà bạn chọn là:',
+    answer: 'C',
     answers: [
       {
         label: 'A',
-        value: 'Lorem A'
+        value: 'Tránh né tình huống, vấn đề.'
       },
       {
         label: 'B',
-        value: 'Lorem B'
+        value: 'Đi theo con đường mà người khác sắp đặt.'
       },
       {
         label: 'C',
-        value: 'Lorem C'
+        value: 'Luôn là chính mình.'
       },
       {
         label: 'D',
-        value: 'Lorem D'
+        value: 'Luôn để tâm đến những lời người khác nói để biến bản thân thành con người mà họ muốn.'
       }
     ]
   },
   {
     id: 'cc34fa8f751a4995b4dbfa4d1d484bbb2',
-    question: 'Lorem ipsum amed',
-    answer: 'B',
+    question: 'Hành động nào trong giao tiếp không làm đúng kĩ năng giao tiếp?',
+    answer: 'A',
     answers: [
       {
         label: 'A',
-        value: 'Lorem A'
+        value: 'Chú ý lắng nghe.'
       },
       {
         label: 'B',
-        value: 'Lorem B'
+        value: 'Chỉ lắng nghe khi cần thiết.'
       },
       {
         label: 'C',
-        value: 'Lorem C'
+        value: 'Nói chen vào lời của người khác khi người ta đang nói.'
       },
       {
         label: 'D',
-        value: 'Lorem D'
+        value: 'Bốc đông, tự cao tự đại.'
       }
     ]
-  },
-  {
-    id: 'cc34fa8f751a4995b4dbfa4d1d484bbb3',
-    question: 'Lorem ipsum amed',
-    answer: 'B',
-    answers: [
-      {
-        label: 'A',
-        value: 'Lorem A'
-      },
-      {
-        label: 'B',
-        value: 'Lorem B'
-      },
-      {
-        label: 'C',
-        value: 'Lorem C'
-      },
-      {
-        label: 'D',
-        value: 'Lorem D'
-      }
-    ]
-  },
+  }
 ]
 
 export default questions
